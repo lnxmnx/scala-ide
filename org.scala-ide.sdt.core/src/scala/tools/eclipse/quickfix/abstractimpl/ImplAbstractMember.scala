14572a674a88ca9aa9a2d5126648818ee6273d91
VladimirNik <vladimir.nikolaev9@gmail.com>
2013-11-27 23:54:42 +0400

    Abstract members impl added

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/quickfix/abstractimpl/ImplAbstractMember.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/quickfix/abstractimpl/ImplAbstractMember.scala
@@ -22,100 +22,57 @@ import scala.tools.eclipse.javaelements.ScalaCompilationUnit
 import scala.tools.eclipse.refactoring.EditorHelpers
 import scala.reflect.internal.util.RangePosition
 import scala.reflect.internal.util.SourceFile
+import scala.reflect.internal.util.NoPosition
 
 object ImplAbstractMember {
   def suggestsFor(ssf: ScalaSourceFile, offset: Int): Array[IJavaCompletionProposal] = {
-    implAbstractMember(ssf, offset).toArray //, compUnit).toArray
+    implAbstractMember(ssf, offset).toArray
   }
 
-  def implAbstractMember(ssf: ScalaSourceFile, offset: Int): List[IJavaCompletionProposal] = { //, compUnit: ICompilationUnit): List[IJavaCompletionProposal] = {
+  def implAbstractMember(ssf: ScalaSourceFile, offset: Int): List[IJavaCompletionProposal] = {
     ssf.withSourceFile { (sourceFile, compiler) =>
       import compiler.{ Tree, ClassDef, ModuleDef, EmptyTree, TypeTree, Type, Symbol, Name }
 
       type NamedTree = Tree { val name: Name; val impl: Tree }
 
-      class AbstractMemberProposal(abstrMember: Symbol, cl: NamedTree, target: AddMethodTarget)  extends IJavaCompletionProposal { //, compilationUnit: ICompilationUnit, pos: Position) extends IJavaCompletionProposal {
-
-        private val UnaryMethodNames = "+-!~".map("unary_" + _)
-
-        //compilationUnit.asInstanceOf[ScalaSourceFile]
-        private val sourceAst = ScalariformParser.safeParse(ssf.getSource()).map(_._1)
-
-//        private def typeAtRange(start: Int, end: Int): String = {
-//          ssf.withSourceFile((srcFile, compiler) => {
-//            compiler.askOption(() => {
-//              val length = end - start
-//              val context = compiler.doLocateContext(new RangePosition(srcFile, start, start, start + length - 1))
-//
-//              val tree = compiler.locateTree(new RangePosition(srcFile, start, start, start + length - 1))
-//              val typer = compiler.analyzer.newTyper(context)
-//              val typedTree = typer.typed(tree)
-//              val tpe = typedTree.tpe.resultType.underlying
-//              if (tpe.isError) None
-//              else if (tpe.toString == "Null") Some("AnyRef") //there must be a better condition
-//              else {
-//                val sprinterType = TypePrinters.showType(compiler, tpe, context)
-//                Some(sprinterType) //do we want tpe.isError? tpe.isErroneous?
-//              }
-//            }).flatten.getOrElse("Any")
-//          }) getOrElse ("Any")
-//        }
-
-        import scala.tools.eclipse.quickfix.createmethod.MissingMemberInfo
-
+      class AbstractMemberProposal(abstrMember: Symbol, cl: NamedTree, target: AddMethodTarget) extends IJavaCompletionProposal {
         private def initValOrDef: (ParameterList, ReturnType) = {
-//          compiler.askOption(() => {
-            def refactContextPos =
-              if (!Option(cl.impl).isEmpty && !cl.impl.isEmpty && !cl.impl.children.isEmpty) {
-                cl.impl.children.last match {
-                  case dd : compiler.DefDef =>
-                    val ndd = dd.name.toString()
-                  case o =>
-                    val otstr = o.toString
-                }
-                cl.impl.children.last.pos
-              } else cl.pos //TODO fix and test it
-            val before = 1
-//            val refactPos = refactContextPos
-//            val refactPosPoint = refactPos.point
-//            val refactRangePos = compiler.rangePos(sourceFile, refactPosPoint, refactPosPoint, refactPosPoint)
-//            val refactRangePosEnd = refactRangePos.end
-//            val clPos = cl.pos
+          //TODO find last printed method
+          //TODO in scala-refactoring we can check with pos.isDefined
+          def refactContextPos =
+            if (!Option(cl.impl).isEmpty && !cl.impl.isEmpty && !cl.impl.children.isEmpty && cl.impl.children.last.pos.isDefined)
+              createPosition(sourceFile, cl.impl.children.last.pos.end)
+            else createPosition(sourceFile, cl.pos.end)
 
-            val scalaFile = cl.pos.source
-            val clPos2isRange = cl.pos.isRange
-            val clPos2 = cl.pos
-            val rangePos2 = compiler.rangePos(scalaFile, cl.pos.end, cl.pos.end, cl.pos.end)
+          //TODO change rangePos and move it to common for all AbstractMemberProposal (it should be one for all)
+          val rangePos = refactContextPos
+          val sprinterContext = compiler.locateContext(rangePos).getOrElse(compiler.doLocateContext(createPosition(sourceFile, offset)))
 
-            val rangePos = compiler.rangePos(sourceFile, cl.pos.end-1, cl.pos.end-1, cl.pos.end-1)
-            val sprinterContext = compiler.locateContext(clPos2)
-            val after = 2
-            def processType(tp: Type) =
-              (if (tp.isError) None
-              else if (tp.toString == "Null") Some("AnyRef") //there must be a better condition
-              else {
-                val sprinterType = TypePrinters.showType(compiler, tp, sprinterContext.getOrElse(null))
-                Some(sprinterType) //do we want tpe.isError? tpe.isErroneous?
-              }) getOrElse ("Any")
+          def processType(tp: Type) =
+            (if (tp.isError) None
+            else if (tp.toString == "Null") Some("AnyRef") //there must be a better condition
+            else {
+              //TODO change to pass Option[Context]
+              val sprinterType = TypePrinters.showType(compiler, tp, sprinterContext)
+              Some(sprinterType) //do we want tpe.isError? tpe.isErroneous?
+            }) getOrElse ("Any")
 
-//            val method = abstrMember.asMethod
-//            val paramss: ParameterList = method.paramss map {
-//              _.map { param =>
-//                (param.name.decode, processType(param.tpe))
-//              }
-//            }
+          val method = abstrMember.asMethod
+          val paramss: ParameterList = method.paramss map {
+            _.map { param =>
+              (param.name.decode, processType(param.tpe))
+            }
+          }
 
-            val paramss: ParameterList = List(List(("x", "Float")))
+          //val paramss: ParameterList = List(List(("x", "Float")))
+          //val retType = Option("Double")
 
-//            val tparams: List[String] = method.typeParams map (_.name.decode)
-            val retType: ReturnType = Option("Double"); //Option(processType(method.returnType.asSeenFrom(cl.symbol.tpe, method.owner)))
-            (paramss, retType)
-//          }).getOrElse((List(Nil), Option("")))
+          val tparams: List[String] = method.typeParams map (_.name.decode)
+          val retType: ReturnType = Option(processType(method.returnType.asSeenFrom(cl.symbol.tpe, method.owner)))
+          (paramss, retType)
         }
 
-        private val (parameters: ParameterList, returnType: ReturnType) = initValOrDef //
-
-        //  def isApplicable = ???
+        private val (parameters: ParameterList, returnType: ReturnType) = initValOrDef
 
         override def apply(document: IDocument): Unit = {
           for {
@@ -159,55 +116,32 @@ object ImplAbstractMember {
         override def getContextInformation: IContextInformation = null
       }
 
-      def implAbstractProposals(tree: NamedTree): List[IJavaCompletionProposal] = {
-        //check tree
-        val list = compiler.askOption { () =>
+      def implAbstractProposals(tree: NamedTree): List[IJavaCompletionProposal] =
+        compiler.askOption { () =>
           val tp = tree.symbol.tpe
-          //TODO check not to display deferred methods local to this class (check owner)
-          val mems = tp.members filter {m =>
-            val misMethod = m.isMethod
-            val misValue = m.isValue
-            val misIncIn = m.isIncompleteIn(tree.symbol)
-            val misDeferred = m.isDeferred
-            val mnisSetter = !m.isSetter
-            val mownneqsym = (m.owner != tree.symbol)
-            val own = m.owner
-            val trsym = tree.symbol
-            (m.isMethod || m.isValue) && m.isIncompleteIn(tree.symbol) && m.isDeferred && !m.isSetter} // && (m.owner != tree.symbol))
-          val res = (mems map {
-            sym =>
+          (tp.members filter { m =>
+            (m.isMethod || m.isValue) && m.isIncompleteIn(tree.symbol) && m.isDeferred && !m.isSetter && (m.owner != tree.symbol)
+          }
+            map { sym =>
               new AbstractMemberProposal(sym, tree, AddToClosest(offset))
-          }).toList
-
-          res
-//        }
-
-//        val fin = list map {
-//          members =>
-//            val res = members map { sym =>
-//              //creation of completion goes here
-//              new AbstractMemberProposal(sym, tree, AddToClosest(offset)) //, compUnit, new Position(offset))
-//            }
-//            res
-//        }
-//        fin getOrElse Nil toList
+            }).toList
         } getOrElse Nil
-        list
-      }
+
+      def createPosition(sf: SourceFile, offset: Int) =
+        compiler.rangePos(sourceFile, offset, offset, offset)
 
       def enclosingClassOrModule(src: SourceFile, offset: Int) =
-        compiler.locateIn(compiler.parseTree(src), compiler.rangePos(src, offset, offset, offset),
-            t => (t.isInstanceOf[ClassDef] || t.isInstanceOf[ModuleDef]))
+        compiler.locateIn(compiler.parseTree(src), createPosition(src, offset),
+          t => (t.isInstanceOf[ClassDef] || t.isInstanceOf[ModuleDef]))
 
       val enclosing = enclosingClassOrModule(sourceFile, offset)
       if (enclosing != EmptyTree) {
-
         compiler.withResponse[Tree] { response =>
           compiler.askTypeAt(enclosing.pos, response)
         }.get.left.toOption flatMap {
-          case cd @ ClassDef(mods, name, tparams, impl) => // if cd.symbol.isAbstractClass =>
+          case cd @ ClassDef(mods, name, tparams, impl) =>
             Option(implAbstractProposals(cd))
-          case md @ ModuleDef(mods, name, impl) => //if md.symbol.isAbstractClass => None
+          case md @ ModuleDef(mods, name, impl) =>
             Option(implAbstractProposals(md))
           case _ => None
         } getOrElse (Nil)
